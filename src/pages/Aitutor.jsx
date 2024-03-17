3bd62a75cf3b204facf62e0fa7f9c5b9f2b52079
Aman Konchgamey <amanmaxwel@gmail.com>
2024-03-18 01:20:34 +0530

    seek to a specific time

--- src/pages/Aitutor.jsx
+++ src/pages/Aitutor.jsx
@@ -1,39 +1,42 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import Layout from '../components/layout/Layout';
 import ReactPlayer from 'react-player';
 
-
 const Aitutor = () => {
-    const handleProgress = (state) => {
-        console.log('onProgress', state)
-        // We only want to update time slider if we are not currently seeking
+  const playerRef = useRef(null);
+  const [seekToTime, setSeekToTime] = useState(0);
 
-      }
+  const handleSeekChange = (e) => {
+    setSeekToTime(parseFloat(e.target.value));
+  }
+
+  const handleSeek = () => {
+    if (playerRef.current) {
+      console.log(seekToTime);
+      playerRef.current.seekTo(seekToTime, 'seconds');
+    }
+  }
 
   return (
     <Layout>
       <div className='mt-8 flex flex-col'>
-     
         <div className='mx-auto'>
-          {/* <iframe
-            id="youtube-player"
-            width="750"
-            height="423"
-            src="https://www.youtube.com/embed/MoqgmWV1fm8?list=PLeo1K3hjS3uu0N_0W6giDXzZIcB07Ng_F"
-            title="LLM Project | End to End LLM Project Using Langchain, OpenAI in Finance Domain"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen>
-          </iframe> */}
-          <ReactPlayer url='https://youtu.be/MoqgmWV1fm8?si=_IfsW2G-oSExXBSB' onProgress={handleProgress} controls/>
+          <ReactPlayer ref={playerRef} url='https://youtu.be/MoqgmWV1fm8?si=_IfsW2G-oSExXBSB' controls />
         </div>
+        
         <div className='mx-24 mt-8'>
           <h2 className='text-2xl'>Any Doubts?</h2>
           <div className='mb-4'>
             <input type="text" placeholder='Ask your doubt ...' className='w-full px-3 py-2 border rounded-lg bg-gray-200 focus:border-blue-500 focus:outline-none' />
+            <button className='p-2 bg-blue-400 px-4 mt-2 text-white'>Submit</button>
           </div>
         </div>
       </div>
+      <div className='mx-24 mt-8'>
+          <h2 className='text-2xl'>Seek to a Specific Time</h2>
+          <input type="number" min={0} step="1" value={String(seekToTime)} onChange={handleSeekChange} />
+          <button onClick={handleSeek}>Seek</button>
+        </div>
     </Layout>
   );
 }
